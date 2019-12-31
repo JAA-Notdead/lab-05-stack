83a49222396be0f085b3d7831413515b39ad57e4
rooted <berkut126@gmail.com>
2019-12-31 19:36:20 +0300

    Initial commit for one with code.

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,45 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+template<typename T>
+class stack{
+public:
+	// Non-copyable
+	stack(const stack&) = delete;
+	// Moveable
+	stack(stack&&) = default;
+	stack() {
+		current = new StackElement();
+		current->prev = nullptr;
+		current->value = T();
+	}
+	void push(T&& value) {
+		auto temp = current;
+		current = new StackElement();
+		current->prev = temp;
+		current->value = std::move(value);
+	}
+	void push(const T& value) {
+		auto temp = current;
+		current = new StackElement();
+		current->prev = temp;
+		current->value = value;
+	}
+	void pop() {
+		if (current->prev != nullptr) {
+			auto temp = current;
+			current = current->prev;
+			delete temp;
+		}
+	}
+	const T& head() const {
+		return current->value;
+	}
+private:
+	struct StackElement {
+		StackElement* prev;
+		T value;
+	} *current;
+};
 
 #endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,16 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include <iostream>
+#include <header.hpp>
 
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+	auto a = stack<int>();
+	a.push(1);
+	a.push(2);
+	a.push(3);
+	a.push(4);
+	std::cout << a.head() << std::endl;
+	a.pop();
+	EXPECT_EQ(3, a.head());
 }
